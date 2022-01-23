01b4193cffdd25962cb640b7eeec6f931ddf225e
Kamal Chavda <kamal.chavda@accounttechnologies.com>
2022-01-23 17:46:41 +0000

    add baseRadioButton

---
+++ src/components/BaseRadioButton.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const emit = defineEmits(['update:modelValue']);
+
+const props = defineProps({
+  option: {
+    type: String,
+    required: true,
+  },
+  selected: {
+    type: [String, Number],
+    default: '',
+  },
+  errorMessage: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <label
+    :for="option"
+    :class="{
+      selected: selected === option,
+      'input-error': errorMessage,
+    }"
+  >
+    <input
+      :id="option"
+      :value="option"
+      type="radio"
+      name="radio-input"
+      @change="
+        (event) => $parent.$emit('update:modelValue', event.target.value)
+      "
+    />
+    {{ option }}
+  </label>
+</template>
+
+<style lang="postcss" scoped>
+label {
+  @apply flex justify-center p-4 mr-2 cursor-pointer border-2 rounded border-gray-500 focus:border-primary-300 bg-gray-200  text-gray-900;
+}
+
+label:last-child {
+  @apply mb-0;
+}
+
+input[name='radio-input'] {
+  @apply hidden;
+}
+
+.selected {
+  @apply bg-primary-300 border-primary-300;
+}
+
+/* .input-error {
+  @apply border-error-200 focus:border-error-200;
+} */
+</style>
---
+++ src/components/BaseRadioButtonGroup.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import BaseRadioButton from '@/components/BaseRadioButton.vue';
+
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+  selected: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  errorMessage: {
+    type: String,
+    default: '',
+  },
+  forceUseOfOptionObject: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-start mb-6">
+    <p class="block pb-2 mb-2 font-medium text-sm">
+      {{ label }}
+    </p>
+    <div class="flex flex-wrap">
+      <BaseRadioButton
+        v-for="option in options"
+        :key="option"
+        :option="option"
+        :selected="selected"
+        :error-message="errorMessage"
+      />
+    </div>
+    <!-- <span v-if="errorMessage" class="error-msg">{{ errorMessage }}</span> -->
+  </div>
+</template>
